a39cb9a113c891b95b70d6d29945ff0ff14c3512
Nurbekabdyrashitovich <nurbekabdyrashitovich@gmail.com>
2023-02-02 21:41:06 +0300

    dz 7

--- products/views.py
+++ products/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from products.models import Product, Review, Category
 from products.forms import ProductCreateForm, ReviewCreateForm
 
+
+PAGINATION_LIMIT = 3
+
 def index(request):
     if request.method == 'GET':
         return render(request, 'layouts/index.html')
@@ -11,7 +14,25 @@ def index(request):
 def all_products(request):
     if request.method == 'GET':
         products = Product.objects.all()
-        return render(request, 'products/products.html', context={'products': products, 'user': request.user})
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
+        if search is not None:
+            products = Product.objects.filter(title__icontains=search)
+
+        max_page = products.__len__() / PAGINATION_LIMIT
+
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products[PAGINATION_LIMIT * (page-1) : PAGINATION_LIMIT * page]
+
+        return render(request, 'products/products.html',
+                      context={'products': products,
+                               'user': request.user,
+                               'max_page': range(1, max_page+1)})
 
 
 
